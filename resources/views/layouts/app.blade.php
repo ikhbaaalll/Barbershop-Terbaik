8771a57bf14272f01afb4307891766403022b168
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-29 15:38:25 +0700

    update master

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('images/icons/TempDonjack.png') }}" sizes="32x32">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Google Font: Source Sans Pro -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,13 +11,13 @@
     <meta name="application-name" content="LAPOR!">
 
     <!-- PAGE TITLE -->
-    <title>{{ config('app.name', 'Donjack Barbershop') }} - {{ $title }} </title>
+    <title>{{ config('app.name', 'Donjack Barbershop') }} </title>
 
     <!-- LINK CSS LAMA DIPINDAH KE BAWAH -->
-    
-    {{-- <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">  --}}
+
+    {{-- <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('css/user_style.css') }}">
-    
+
 </head>
 
 <body class="hold-transition login-page">
@@ -41,6 +42,7 @@
     <!-- AdminLTE App -->
     <script src="{{ asset('js/adminlte.min.js') }}"></script>
 </body>
+
 </html>
 
 
@@ -49,14 +51,10 @@
 <!-- LINK CSS LAMA -->
 
 <!-- Google Font: Source Sans Pro -->
-{{-- 
-        
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+{{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('css/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}"> 
-    
---}}
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}"> --}}
