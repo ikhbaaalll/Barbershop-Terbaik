8e6d8ee5e07298721a5ae36875b3e603a20d013e
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-30 23:51:05 +0700

    merge web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\CapsterController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('canvas', function(){
+Route::get('canvas', function () {
     return view('pages.user.canvas');
 });
 
@@ -21,14 +22,15 @@ Route::post('register', [RegisterController::class, 'register'])->name('register
 
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => ['auth', 'admin'], 'as' => 'admin.', 'prefix' => 'admin'], function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
+    Route::resource('capsters', CapsterController::class)->except('show');
 });
 
 Route::group(['middleware' => ['auth', 'user'], 'as' => 'user.'], function () {
-    Route::get('home',      [UserController::class, 'index'  ])->name('home');
+    Route::get('home',      [UserController::class, 'index'])->name('home');
     Route::get('profile',   [UserController::class, 'profile'])->name('profile');
-    Route::get('about',     [UserController::class, 'about'  ])->name('about');
+    Route::get('about',     [UserController::class, 'about'])->name('about');
 });
