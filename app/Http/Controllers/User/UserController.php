6ba8c6425f8d05ccf3a0ad02a414365c404a497d
ikhbaaalll <ikhbal.madrid17@gmail.com>
2022-01-02 04:07:49 +0700

    update UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\BookingRequest;
 use App\Models\Barber;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -23,20 +25,69 @@ class UserController extends Controller
 
         return view('pages.user.detail', compact('barber'));
     }
-    public function capster()
+    public function capster(Barber $barber)
     {
-        return view('pages.user.capster');
+        $barber = Barber::query()
+            ->with('capsters')
+            ->withCount('capsters')
+            ->find($barber->id);
+
+        return view('pages.user.capster', compact('barber'));
     }
-    public function booking()
+    public function booking(Barber $barber)
     {
-        return view('pages.user.booking');
+        $barber = Barber::query()
+            ->with('capsters')
+            ->withCount('capsters')
+            ->find($barber->id);
+
+        return view('pages.user.booking', compact('barber'));
     }
+
+    public function bookingStore(BookingRequest $bookingRequest, Barber $barber)
+    {
+        $barber->orders()->create($bookingRequest->validated() + [
+            'user_id' => auth()->id()
+        ]);
+
+        return redirect()->route('user.profile');
+    }
+
     public function profile()
     {
-        return view('pages.user.profile');
+        $orders = Order::query()
+            ->with(['capster', 'barber'])
+            ->whereBelongsTo(auth()->user())
+            ->get();
+
+        return view('pages.user.profile', compact('orders'));
     }
+
     public function about()
     {
         return view('pages.user.about');
+    }
+
+    public function review(Order $order)
+    {
+        $validator = validator()->make(request()->all(), [
+            'review_star' => ['required', 'in:1,2,3,4,5', 'integer'],
+            'review_text' => ['nullable']
+        ]);
+
+        $order->update($validator->validated() + [
+            'status' => Order::STATUS_REVIEWED
+        ]);
+
+        return redirect()->route('user.profile');
+    }
+
+    public function cancel(Order $order)
+    {
+        $order->update([
+            'status' => Order::STATUS_CANCEL
+        ]);
+
+        return redirect()->route('user.profile');
     }
 }
